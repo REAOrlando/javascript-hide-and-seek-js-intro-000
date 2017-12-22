8735fe50ba8873d463565e9fe33481fd1520e524
Chris Albanese <chris@reaorlando.com>
2017-12-22 19:47:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ function getFirstSelector(selector) {
   return document.querySelector(selector)
 }
 
-function nestedTarget(target, nested) {
-  return document.querySelector(target nested)
+// function nestedTarget(target, nested) {
+//   return document.querySelector(target nested)
+// }
+
+function increaseRankBy(n) {
+  const lis = document.getElementById("app").querySelectorAll("ul.ranked-list li")
+  for (var i = 0; i < const.length; i++) {
+    const[i].innerHTML = parseInt(const[i].innerHTML) + n
+  }
 }
